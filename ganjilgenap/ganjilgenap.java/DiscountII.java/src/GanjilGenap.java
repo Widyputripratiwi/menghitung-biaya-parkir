4dc267af6c8ffef719df96135019b8f36be262ed
unknown <widy15179@gmail.com>
2023-10-13 14:53:17 +0700

    update tugas

---
+++ ganjilgenap/ganjilgenap.java/DiscountII.java/src/GanjilGenap.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class GanjilGenap {
+    public class Ganjil_Genap {
+    public static void main(String[] args) throws Exception {
+        try (Scanner inputan = new Scanner(System.in)) {
+            System.out.print("Masukkan Angka : ");
+            int Angka = inputan.nextInt();
+            
+            if (Angka % 2 == 0) {
+                System.out.println("Bilangan Genap");            
+            } else {
+                System.out.println("Bilangan Ganjil");
+                
+            
+        }
+    }
+        
+    }
+}
+}
---
+++ tugas.jodoh.iqbaal/DiscountII.java/src/Discount.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Discount {
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        Double totalbelanja, Diskon;
+
+        try (Scanner inputan = new Scanner(System.in)) {
+            System.out.print("jumlah belanja: ");
+            totalbelanja = inputan.nextDouble();
+        }
+        if (totalbelanja >= 100000) {
+           Diskon = 90/100 * totalbelanja;
+           totalbelanja = totalbelanja - Diskon;
+        }
+
+        System.out.println("total yang harus dibayar: " + totalbelanja);
+    }
+}
